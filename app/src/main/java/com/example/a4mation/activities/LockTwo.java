071e2cff164e1aa3f045520b3580414093b1e98f
SupunGurusinghe <supunsameeran@gmail.com>
2021-09-20 22:05:06 +0530

    Change Action Bar

--- app/src/main/java/com/example/a4mation/activities/LockTwo.java
+++ app/src/main/java/com/example/a4mation/activities/LockTwo.java
@@ -1,14 +1,19 @@
 package com.example.a4mation.activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -50,6 +55,33 @@ public class LockTwo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lock_two);
+
+
+        // Define ActionBar object
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+
+        // Define ColorDrawable object and parse color
+        // using parseColor method
+        // with color hash code as its parameter
+        ColorDrawable colorDrawable
+                = new ColorDrawable(Color.parseColor("#DC143C"));
+
+        // Set BackgroundDrawable
+        actionBar.setBackgroundDrawable(colorDrawable);
+
+        // Change Toolbar text
+        getSupportActionBar().setTitle("Lock Password");
+        // getSupportActionBar().setSubtitle("Main");
+
+        // Change the color of status bar
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.colorStatusBar));
+        }
+
 
         title = findViewById(R.id.InputPasswordTitle);
         password = findViewById(R.id.inputPassword);
