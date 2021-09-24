24a07099b06427e2ec70dcf46f71eb9b91a9a6fb
Nethmi Amanda <86509762+nethmiamanda99@users.noreply.github.com>
2021-09-25 04:53:46 +0530

    Create list Layout

--- app/src/main/java/com/example/a4mation/activities/ListView.java
+++ app/src/main/java/com/example/a4mation/activities/ListView.java
@@ -1,16 +1,118 @@
 package com.example.a4mation.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.a4mation.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class ListView extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class ListView  extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FloatingActionButton add_button;
+
+    DbHandler myDB;
+    ArrayList<String> item_id, item_name,item_quentity;
+    ListCustomAdapter myCustomAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_view);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        add_button = findViewById(R.id.add_button);
+        add_button.setOnClickListener(view -> {
+            Intent intent = new Intent(ListView .this, AddItems.class);
+            startActivity(intent);
+        });
+
+        myDB = new DbHandler(ListView.this);
+        item_id = new ArrayList<>();
+        item_name = new ArrayList<>();
+        item_quentity = new ArrayList<>();
+
+        storeDataInArrays();
+
+        myCustomAdapter = new ListCustomAdapter(ListView.this, this, item_id, item_name, item_quentity);
+        recyclerView.setAdapter(myCustomAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(ListView .this));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 0){
+            recreate();
+        }
+    }
+
+    void storeDataInArrays(){
+        Cursor cursor = myDB.readAllData();
+        if(cursor.getCount() == 0){
+            Toast.makeText(this,"No data", Toast.LENGTH_SHORT).show();
+        }else{
+            while (cursor.moveToNext()){
+                item_id.add(cursor.getString(0));
+                item_name.add(cursor.getString(1));
+                item_quentity.add(cursor.getString(2));
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.list_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.delete_all){
+            confirmDialog();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    void confirmDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete All");
+        builder.setMessage("Are you sure you want to delete all Data?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                DbHandler myDB = new DbHandler(ListView .this);
+                myDB.deleteAllData();
+                //Refresh Activity
+                Intent intent = new Intent(ListView .this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        builder.create().show();
     }
 }
